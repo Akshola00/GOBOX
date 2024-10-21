e689941ac544bb425c5bffd09603b8f161600aab
Akshola00 <akinniyishola07@gmail.com>
2024-10-21 18:37:29 +0100

    chore: some updates

--- components/GetTicketButton.tsx
+++ components/GetTicketButton.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Button } from './ui/button'
 import { Lato } from 'next/font/google';
+import Link from 'next/link';
 // background: #765BFF;
 
 const lato = Lato({ weight: ["400"], subsets: ["latin"] });
 const GetTicketButton = () => {
   return (
-    <div className='w-full flex items-center justify-center'>
+    <Link href="/EventRegForm" className='w-full flex items-center justify-center'>
     <Button className={` ${lato.className} text-center items-center bg-[#765BFF] h-[50px] w-[90%] rounded-[73px]  `} >
         Get Ticket
     </Button>
-    </div>
+    </Link>
   )
 }
 
